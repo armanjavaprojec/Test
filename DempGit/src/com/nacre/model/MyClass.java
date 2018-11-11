969db62c13efcdbbcaefc9cb4b4408236c8e59c1
Armaan <Armaan@192.168.0.5>
2018-11-11 12:50:56 +0530

    Added m2()methods

--- DempGit/src/com/nacre/model/MyClass.java
+++ DempGit/src/com/nacre/model/MyClass.java
@@ -6,5 +6,12 @@ public class MyClass {
 		System.out.println("Hello Arman Test2 done");
 		System.out.println("Hello Arman Test3 done");
 	}
+	
+	public static  void m2() {
+		System.out.println("Hello Arman Test1 done");
+		System.out.println("Hello Arman Test2 done");
+		System.out.println("Hello Arman Test3 done");
+	}
+	
 
 }
