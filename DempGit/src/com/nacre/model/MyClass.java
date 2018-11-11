6468a4faf1f6110fceb06834766d75b25040ea85
Armaan <Armaan@192.168.0.5>
2018-11-11 11:19:13 +0530

    Add simple change on Test2.

--- DempGit/src/com/nacre/model/MyClass.java
+++ DempGit/src/com/nacre/model/MyClass.java
@@ -2,7 +2,8 @@ package com.nacre.model;
 
 public class MyClass {
 	public static  void m1() {
-		System.out.println("Hello Arman Test done");
+		System.out.println("Hello Arman Test1 done");
+		System.out.println("Hello Arman Test2 done");
 	}
 
 }
