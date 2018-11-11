0ef2c4e02e309de5a1daf9c3f1b824dce155dd9c
Armaan <Armaan@192.168.0.5>
2018-11-11 10:59:00 +0530

    Initial Test1.

---
+++ DempGit/src/com/nacre/model/MyClass.java
@@ -0,0 +1,8 @@
+package com.nacre.model;
+
+public class MyClass {
+	public static  void m1() {
+		System.out.println("Hello Arman Test done");
+	}
+
+}
---
+++ DempGit/src/com/nacre/test1/Test.java
@@ -0,0 +1,10 @@
+package com.nacre.test1;
+
+import com.nacre.model.MyClass;
+
+public class Test {
+	public static void main(String[] args) {
+		MyClass.m1();
+	}
+
+}
